719a68306108df7c3722c7ccb21e156ad0346c8b
IIapad0ks <bohdan.lubenets@gmail.com>
2015-05-29 19:50:50 +0300

    add BackupManager

---
+++ BackupManager/AmazonS3Settings.cs
@@ -0,0 +1,37 @@
+﻿using Core.Settings;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settings
+{
+    public class AmazonS3Settings : IAmazonS3Settings
+    {
+        public string AccessKey
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["AmazonS3AccessKey"];
+            }
+        }
+
+        public string SecretAccessKey
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["AmazonS3SecretAccessKey"];
+            }
+        }
+
+        public string BucketName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["AmazonS3BucketName"];
+            }
+        }
+    }
+}
---
+++ BackupManager/BackupSettings.cs
@@ -0,0 +1,70 @@
+﻿using Core.Settings;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupManager
+{
+    class BackupSettings : IBackupSettings
+    {
+        public string ServerName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupServerName"];
+            }
+        }
+
+        public string DatabaseName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupDatabaseName"];
+            }
+        }
+
+        public string BackupPath
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupPath"];
+            }
+        }
+
+        public bool LoginSecure
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupServerLoginSecure"].ToLower() == "true";
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupServerLogin"];
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["BackupServerPassword"];
+            }
+        }
+
+
+        public bool RestoreLocalBackup
+        {
+            get 
+            {
+                return ConfigurationManager.AppSettings["RestoreLocalBackup"].ToLower() == "true";
+            }
+        }
+    }
+}
